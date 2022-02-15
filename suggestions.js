9cc3386b41fc6d9b36185dde6be1d241e1716831
Smok Codes <99590709+SmokCodes@users.noreply.github.com>
2022-02-15 23:08:15 +0530

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,22 @@
+// Created By Smok Codes
+// These are all the suggestions for js to bring up in the search bar
+let suggestions = [
+    "<li>How to become a web developer</li>",
+    "<li>How to become a software engineer</li>",
+    "<li>How to become a doctor</li>",
+    "<li>How to earn money online</li>",
+    "<li>how to get a lot of subscribers on youtube</li>",
+    "<li>what is html</li>",
+    "<li>What is css</li>",
+    "<li>What is Java Script</li>",
+    "<li>How to complete 400 watchhours very fast</li>",
+    "<li>how to make a bot with python</li>",
+    "<li>youtube</li>",
+    "<li>Google</li>",
+    "<li>stackoverflow</li>",
+    "<li>geeks for geeks</li>",
+    "<li>facebook</li>",
+    "<li>twitter</li>",
+    "<li>my space</li>",
+    "<li>google translate</li>",
+];
